b9dffcf86324839eb1c172e839c31870d5507d2a
aleconte <aleconte@student.42.fr>
2023-03-28 09:11:56 -0400

    interrupt descriptor, idt_ptr

--- src/idt/idt.c
+++ src/idt/idt.c
@@ -1,5 +1,8 @@
 #include "idt.h"
 
+struct idt_interrupt_desc idt[256];
+struct idt_ptr idt_ptr;
+
 void init_idt() {
 
 }
--- src/idt/idt.h
+++ src/idt/idt.h
@@ -1,6 +1,21 @@
 #ifndef IDT_H
 #define IDT_H
 
+#include <stdint.h>
+
+struct idt_interrupt_desc {
+   uint16_t base_low; /// address of the ISR (interrupt service routine) to jump to when this interrupt is called
+   uint16_t segment_selector;  ///valid gdt segment               
+   uint8_t  reserved; /// set to 0 
+   uint8_t  access;               
+   uint16_t base_high; /// address is divided in two parts
+} __attribute__((packed));
+
+struct idt_ptr {
+   uint16_t limit; /// size of idt
+   uint32_t base; /// address
+} __attribute__((packed));
+
 void init_idt();
 
 #endif
